fc534fa85f5ea84b132d9a99eea4d8ba7f98bd06
Min Huang <70873102+min0625@users.noreply.github.com>
2025-12-16 20:35:26 +0800

    chore: Integrate asdf package manager

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Bashrc setup
+echo '' >>~/.bashrc
+echo 'source /workspaces/TalkRealm/.devcontainer/bashrc.bash' >>~/.bashrc
+
+# Install apt utility
 sudo apt update
 sudo apt install -y iputils-ping
+
+# Install ASDF
+go install github.com/asdf-vm/asdf/cmd/asdf@v0.18.0
+
+# ASDF Package Manager Plugins
+asdf plugin add golang
+asdf plugin add golangci-lint
+asdf plugin add swag
